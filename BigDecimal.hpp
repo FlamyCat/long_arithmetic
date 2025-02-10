ca23f2f393b0fbd0010669f73e7ce4d9c8806cfc
Александр Сергиенко <s_sandr06@mail.ru>
2025-02-11 00:26:09 +0300

    Header file created

---
+++ BigDecimal.hpp
@@ -0,0 +1,70 @@
+#ifndef LONG_ARITHMETIC_BIGDECIMAL_HPP
+#define LONG_ARITHMETIC_BIGDECIMAL_HPP
+
+#include <cstdlib>
+#include <deque>
+#include <string>
+#include <compare>
+#include <cstdint>
+
+class BigDecimal
+{
+    std::deque<uint32_t> _chunks;
+    size_t _floatingPointPosition;
+    signed char _sign;
+
+public:
+    // Constructors
+    BigDecimal(std::string& s);
+
+    BigDecimal(const BigDecimal& other) = default;
+
+    // Math operators
+    BigDecimal& operator+(BigDecimal& other);
+
+    BigDecimal& operator-(BigDecimal& other);
+
+    BigDecimal& operator*(BigDecimal& other);
+
+    BigDecimal& operator/(BigDecimal& other);
+
+    BigDecimal& operator+=(BigDecimal& other);
+
+    BigDecimal& operator-=(BigDecimal& other);
+
+    BigDecimal& operator*=(BigDecimal& other);
+
+    BigDecimal& operator/=(BigDecimal& other);
+
+    // Assignment
+    BigDecimal& operator=(const BigDecimal& other);
+
+    // Comparison operators
+    std::strong_ordering operator<=>(const BigDecimal& other) const;
+
+    bool operator<=(const BigDecimal& other);
+
+    bool operator>=(const BigDecimal& other);
+
+    bool operator<(const BigDecimal& other);
+
+    bool operator>(const BigDecimal& other);
+
+    bool operator==(const BigDecimal& other);
+
+    // Methods
+    void setPrecision(size_t newPrecision);
+
+    void trim();
+
+    // Accessors
+    signed char sign() const;
+    size_t size();
+    size_t floatingPointPosition() const;
+
+    ~BigDecimal() = default;
+};
+
+BigDecimal& operator ""_longnum(long double number);
+
+#endif //LONG_ARITHMETIC_BIGDECIMAL_HPP
