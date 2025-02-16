86c6de392b7e832f365d9334ba6a9c9f20f463bb
Александр Сергиенко <s_sandr06@mail.ru>
2025-02-16 23:42:58 +0300

    Addition tests + DSL improvements

--- test/addition.cpp
+++ test/addition.cpp
@@ -1,27 +1,156 @@
 #include "TestResult.h"
 #include "../src/BigDecimal.hpp"
+#include "macros.hpp"
+#include <bitset>
 
-TestResult emptyAddEmpty() {
-    auto emptyLhs = BigDecimal();
-    auto emptyRhs = BigDecimal();
+testGroup(Addition, {
+    test(emptyAddEmpty, {
+            auto lhs = BigDecimal();
+            auto rhs = BigDecimal();
 
-    emptyLhs += emptyRhs;
+            lhs += rhs;
 
-    if (emptyLhs.size() != 0) {
-        return {false, "Empty + empty != empty"};
-    }
+            check(LhsSign, 1, lhs.sign(), "Lhs sign changed")
+            check(RhsSign, 1, rhs.sign(), "Rhs sign changed")
 
-    if (emptyLhs.sign() != 1) {
-        return {false, "Empty + empty resulted in sign change"};
-    }
+            check(LhsSize, 0, lhs.size(), "Lhs size changed")
+            check(RhsSize, 0, rhs.size(), "Rhs size changed")
 
-    if (emptyRhs.size() != 0) {
-        return {false, "Empty + empty resulted in rhs being not empty"};
-    }
+            ok
+    })
 
-    if (emptyRhs.sign() != 1) {
-        return {false, "Empty + empty resulted in rhs sign change"};
-    }
+    test(intAddEmpty, {
+            auto lhs = 1.0_longnum;
+            auto rhs = BigDecimal();
 
-    return {true, ""};
-}
+            lhs += rhs;
+
+            check(LhsSign, 1, lhs.sign(), "Lhs sign changed")
+            check(RhsSign, 1, rhs.sign(), "Rhs sign changed")
+
+            check(LhsSize, 1, lhs.size(), "Lhs size changed")
+            check(RhsSize, 0, rhs.size(), "Rhs size changed")
+
+            check(LhsFirstChunk, 1, lhs.chunks()[0], "Lhs first chunk changed")
+
+            ok
+    })
+
+    test(negativeIntAddEmpty, {
+            BigDecimal lhs{ "-1.0" };
+
+            auto rhs = BigDecimal();
+
+            lhs += rhs;
+
+            check(LhsSign, -1, lhs.sign(), "Lhs sign changed")
+            check(RhsSign, 1, rhs.sign(), "Rhs sign changed")
+
+            check(LhsSize, 1, lhs.size(), "Lhs size changed")
+            check(RhsSize, 0, rhs.size(), "Rhs size changed")
+
+            check(LhsFirstChunk, 1, lhs.chunks()[0], "Lhs first chunk changed")
+
+            ok
+    })
+
+    test(floatAddEmpty, {
+            auto lhs = 1.2_longnum;
+
+            auto rhs = BigDecimal();
+
+            lhs += rhs;
+
+            check(LhsSign, 1, lhs.sign(), "Lhs sign changed")
+            check(RhsSign, 1, rhs.sign(), "Rhs sign changed")
+
+            check(LhsSize, 2, lhs.size(), "Lhs size changed")
+            check(RhsSize, 0, rhs.size(), "Rhs size changed")
+
+            check(LhsFirstChunk, 1, lhs.chunks()[0], "Lhs first chunk changed")
+            check(LhsSecondChunk, 2, lhs.chunks()[1], "Lhs second chunk changed")
+
+            ok
+    })
+
+    test(floatAddFloatSamePrecision, {
+            auto lhs = 1.2_longnum;
+            auto rhs = 3.1_longnum;
+
+            lhs += rhs;
+
+            check(LhsSign, 1, lhs.sign(), "Lhs sign changed")
+            check(RhsSign, 1, rhs.sign(), "Rhs sign changed")
+
+            check(LhsSize, 2, lhs.size(), "Lhs size changed")
+            check(RhsSize, 2, rhs.size(), "Rhs size changed")
+
+            check(LhsIntPart, 4, lhs.chunks()[1], "Lhs: incorrect int part")
+            check(LhsFloatPart, 3, lhs.chunks()[0], "Lhs: incorrect float part")
+
+            check(RhsIntPart, 3, rhs.chunks()[1], "Rhs: incorrect int part")
+            check(RhsFloatPart, 1, lhs.chunks()[0], "Rhs: incorrect float part")
+
+            ok
+    })
+
+    test(floatAddLhsMorePrecise, {
+            auto lhs = 1.21_longnum;
+            auto rhs = 1.1_longnum;
+
+            lhs += rhs;
+
+            lhsRhsCheck(Sign, 1, 1, sign(), "sign changed")
+
+            lhsRhsCheck(FirstChunk, 1, 1, chunks()[0], "incorrect first chunk")
+            lhsRhsCheck(SecondChunk, 3, 1, chunks()[1], "incorrect second chunk")
+            check(LhsThirdChunk, 2, lhs.chunks()[2], "Lhs: Incorrect third chunk")
+
+            ok
+    })
+
+    test(floatAddRhsMorePrecise, {
+            auto lhs = 1.1_longnum;
+            auto rhs = 1.21_longnum;
+
+            lhs += rhs;
+
+            lhsRhsCheck(Sign, 1, 1, sign(), "sign changed")
+
+            lhsRhsCheck(FirstChunk, 1, 1, chunks()[0], "incorrect first chunk")
+            lhsRhsCheck(SecondChunk, 3, 2, chunks()[1], "incorrect second chunk")
+            lhsRhsCheck(ThirdChunk, 2, 1, chunks()[2], "incorrect third chunk")
+
+            ok
+    })
+
+    test(differentSignAdditionBecomesSubtraction, {
+            auto lhs = 1.3_longnum;
+            auto rhs = BigDecimal("-0.1");
+
+            lhs += rhs;
+
+            lhsRhsCheck(Sign, 1, -1, sign(), "incorrect sign")
+
+            lhsRhsCheck(FirstChunk, 2, 1, chunks()[0], "incorrect first chunk")
+            lhsRhsCheck(SecondChunk, 1, 0, chunks()[1], "incorrect second chunk")
+
+            ok
+    })
+
+    test(overflowWorksCorrectly, {
+            auto lhsVal = std::to_string(UINT32_MAX - 1);
+
+            auto lhs = BigDecimal(lhsVal);
+            auto rhs = 1.0_longnum;
+
+            lhsRhsCheck(Size, 2, 1, size(), "incorrect size")
+
+            lhsRhsCheck(Sign, 1, 1, sign(), "incorrect sign")
+
+            lhsRhsCheck(FirstChunk, 0, 0, chunks()[0], "incorrect first chunk")
+            lhsRhsCheck(SecondChunk, 1, 1, chunks()[1], "incorrect second chunk")
+
+            ok
+    })
+})
--- test/macros.hpp
+++ test/macros.hpp
@@ -1,20 +1,52 @@
-//
-// Created by Aleksandr on 16.02.25.
-//
-
 #ifndef LONG_ARITHMETIC_MACROS_HPP
 #define LONG_ARITHMETIC_MACROS_HPP
 
+#include <format>
+#include <iostream>
+
 #define fail return {false, description};
 #define ok return {true, ""};
-#define description(dsc, exp, act) auto description = std::format(R"(dsc + ": expected: "{}", actual: "{}")", exp, act)
-#define faild(dsc, exp, act) description(dsc, exp, act); fail
-#define faildea(dsc, val) faild(dsc, expected##val, actual##val)
-#define check(title, exp, act, dsc) \
-    const auto expected##title = exp; \
-    const auto actual##title = act;   \
+
+#define description(dsc, exp, act) \
+    auto description = dsc + std::format(": expected: \"{}\", actual: \"{}\"", exp, act)
+
+#define faild(dsc, exp, act) \
+    description(dsc, exp, act); fail
+
+#define faildea(dsc, val) \
+    faild(dsc, expected##val, actual##val)
+
+#define check(title, exp, act, dsc)         \
+    const auto expected##title = exp;       \
+    const auto actual##title = act;         \
     if (expected##title != actual##title) { \
-        faildea(dsc, title)         \
+        faildea(dsc, title)                 \
+    }
+
+#define lhsRhsCheck(title, lhsExp, rhsExp, act, dsc)                                        \
+    check(Lhs##title, lhsExp, lhs.act, (std::string)"Lhs: " + (std::string)dsc)             \
+    check(Rhs##title, rhsExp, rhs.act, (std::string)"Rhs: " + (std::string)dsc)             \
+
+#define test(title, body)                                                  \
+    {                                                                      \
+        std::cout << "Running test " << #title << " : ";                   \
+        TestResult _res = ([]() -> TestResult body)();                     \
+        if (_res.success) {                                                \
+            std::cout << "OK" << std::endl;                                \
+        } else {                                                           \
+            std::cout << "FAIL\n";                                         \
+            std::cout << "Description: " << _res.description << std::endl; \
+        }                                                                  \
+        std::cout << "-----------------------------------------------\n";  \
+    }
+
+#define testGroup(title, body)                                                        \
+    void test##title() {                                                              \
+        const std::string _sep = "===============================================\n"; \
+        std::cout << _sep;                                                            \
+        std::cout << "Running " << #title << " test group";                           \
+        std::cout << _sep;                                                            \
+        body                                                                          \
     }
 
 #endif //LONG_ARITHMETIC_MACROS_HPP
