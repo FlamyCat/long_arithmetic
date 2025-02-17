ca3e3947aa5c5844a90a3c2257f2ece05a0ca4f5
Александр Сергиенко <s_sandr06@mail.ru>
2025-02-17 18:49:04 +0300

    Rewritten ctor tests to fully utilize test DSL

--- test/constructor.cpp
+++ test/constructor.cpp
@@ -5,112 +5,71 @@
 #include "../src/BigDecimal.hpp"
 #include "macros.hpp"
 
-TestResult integerFromSingleChunk() {
-    BigDecimal bd{"10"};
+testGroup(Constructor, {
+    skip(testMethod(integerFromSingleChunk, {
+            BigDecimal bd{ "10" };
 
-    auto chunks = bd.chunks();
+            auto chunks = bd.chunks();
 
-    const auto expectedNumberOfDigits = 1;
-    if (bd.size() != expectedNumberOfDigits) {
-        faild("BigDecimal string constructor constructed unexpected number of arguments", expectedNumberOfDigits,
-              bd.size())
-    }
+            check(NumberOfDigits, 1, bd.size(), "Incorrect number of chunks")
 
-    auto firstDigit = bd.chunks()[0];
+            check(Chunk, 2, bd.chunks()[0], "Incorrect chunk")
 
-    const int expectedDigit = 2;
-    if (firstDigit != expectedDigit) {
-        faild("String constructor of BigDecimal produces incorrect chunks", expectedDigit, firstDigit)
-    }
+            ok
+    }))
 
-    ok
-}
+    skip(testMethod(integerFromMultipleChunks, {
+            auto bitset = std::bitset<33>(1);
+            bitset <<= 32;
 
-TestResult integerFromMultipleChunks() {
-    auto str = std::bitset<33>(257).to_string();
+            auto str = bitset.to_string();
 
-    BigDecimal bd{str};
+            BigDecimal bd{ str };
 
-    auto chunks = bd.chunks();
+            auto chunks = bd.chunks();
 
-    const auto expectedNumberOfChunks = 2;
-    const auto actualNumberOfChunks = chunks.size();
-    if (actualNumberOfChunks != expectedNumberOfChunks) {
-        faildea("String constructor produces incorrect number of chunks", NumberOfChunks);
-    }
+            check(NumberOfChunks, 2, chunks.size(), "Incorrect number of chunks")
 
-    const auto expectedFirstChunk = 0;
-    const auto actualFirstChunk = chunks[0];
-    if (actualFirstChunk != expectedFirstChunk) {
-        faildea("First chunk is incorrect", FirstChunk)
-    }
+            check(FirstChunk, 0, chunks[0], "Incorrect first chunk")
+            check(SecondChunk, 1, chunks[1], "Incorrect second chunk")
 
-    const auto expectedSecondChunk = 2;
-    const auto actualSecondChunk = chunks[1];
-    if (actualSecondChunk != expectedSecondChunk) {
-        faildea("Second chunk is incorrect", SecondChunk)
-    }
+            ok
+    }))
 
-    ok
-}
+    skip(testMethod(negativeInteger, {
+            auto str = '-' + (std::bitset<33>(1) << 32).to_string();
 
-TestResult negativeInteger() {
-    auto str = '-' + std::bitset<33>(257).to_string();
+            BigDecimal bd{ str };
 
-    BigDecimal bd{str};
+            auto chunks = bd.chunks();
 
-    auto chunks = bd.chunks();
+            check(NumberOfChunks, 2, chunks.size(), "Incorrect number of chunks")
 
-    const auto expectedNumberOfChunks = 2;
+            check(FirstChunk, 0, chunks[0], "Incorrect first chunk")
+            check(SecondChunk, 1, chunks[1], "Incorrect second chunk")
 
-    const auto actualNumberOfChunks = chunks.size();
-    if (actualNumberOfChunks != expectedNumberOfChunks) {
-        faildea("String constructor produces incorrect number of chunks", NumberOfChunks)
-    }
+            ok
+    }))
 
-    const auto expectedFirstChunk = 0;
-    const auto actualFirstChunk = chunks[0];
-    if (actualFirstChunk != expectedFirstChunk) {
-        faildea("First chunk is incorrect", FirstChunk)
-    }
+    skip(testMethod(floatingPointNumber, {
+            const auto intPart = "-11.";
+            auto floatPart = std::bitset<32>(2).to_string();
 
-    const auto expectedSecondChunk = 2;
-    const auto actualSecondChunk = chunks[1];
-    if (actualSecondChunk != expectedSecondChunk) {
-        faildea("Second chunk is incorrect", SecondChunk)
-    }
+            // Let's check the padding
+            floatPart.pop_back();
 
-    ok
-}
+            // Expected to equal -3.2
+            BigDecimal bd{ intPart + floatPart };
 
-TestResult floatingPointNumber() {
-    const auto intPart = "-11.";
-    auto floatPart = std::bitset<32>(2).to_string();
+            auto chunks = bd.chunks();
 
-    // Let's check the padding
-    floatPart.pop_back();
+            check(NumberOfChunks, 2, chunks.size(), "Incorrect number of chunks")
 
-    // Expected to equal -2.2
-    BigDecimal bd{intPart + floatPart};
+            check(Sign, -1, bd.sign(), "Incorrect sign")
 
-    auto chunks = bd.chunks();
+            check(FirstChunk, 2, chunks[0], "First chunk doesn't match")
+            check(SecondChunk, 3, chunks[1], "Second chunk doesn't match")
 
-    check(NumberOfChunks, 2, chunks.size(), "Incorrect number of chunks")
-
-    check(Sign, -1, bd.sign(), "Incorrect sign")
-
-    check(FirstChunk, 2, chunks[0], "First chunk doesn't match")
-    check(SecondChunk, 3, chunks[1], "Second chunk doesn't match")
-
-    ok
-}
-
-TestResult fromDouble() {
-    auto bd = 2.1_longnum;
-
-    check(Sign, 1, bd.sign(), "Sign doesn't match")
-    check(FirstChunk, 1, bd.chunks()[0], "First chunk doesn't match")
-    check(SecondChunk, 2, bd.chunks()[1], "Second chunk doesn't match")
-
-    ok
-}
+            ok
+    }))
+})
