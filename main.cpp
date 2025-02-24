a8917facae3ac63875e5713d8d541e12a7a89c51
Александр Сергиенко <s_sandr06@mail.ru>
2025-02-24 23:03:36 +0300

    Populated main with test calls

--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+#include "test/addition.hpp"
+#include "test/constructor.hpp"
+#include "test/subtraction.hpp"
+#include "test/multiplication.hpp"
+#include "test/division.hpp"
+
 int main() {
+     testConstructor();
+     testAddition();
+     testSubtraction();
+     testMultiplication();
+     testDivision();
+
     return 0;
 }
